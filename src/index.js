96dfa5d5d4db8160e3f5dfa1afbf028dddac3a9d
Gracysinngh <gracysinngh27.com>
2026-01-20 14:48:59 +0530

    Used express to build the server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-const http = require('http');
-let server=http.createServer((req,res)=>{
-    console.log("Server is runnig on http://localhost:3000")
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.end('Hello, World!');
+// const http = require('http');
+// let server=http.createServer((req,res)=>{
+//     console.log("Server is runnig on http://localhost:3000")
+//     res.writeHead(200,{'content-type':'text/plain'});
+//     res.end('Hello, World!');
 
     
+// });
+
+// server.listen(3000);
+
+
+
+// src/index.js
+
+// 1. Import the express library
+const express = require('express');
+
+// 2. Create an instance of an Express application
+const app = express();
+
+// 3. Define the port
+const PORT = 3000;
+
+// 4. Create a basic GET route for the root path
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blogify API!');
 });
 
-server.listen(3000);
+// 5. Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
